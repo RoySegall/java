280ad40b8ade0b4e29a6dffee242037d859b4f8c
Roy Segall <roysegall@Roys-MacBook-Pro-2.local>
2014-03-14 23:52:38 +0200

    Done with the home work.

--- src/Einstein.java
+++ src/Einstein.java
@@ -4,13 +4,19 @@ public class Einstein {
 
   public static void main(String args[]) {
     Scanner scan = new Scanner(System.in);
-    int Number, ReversedNumber, Difference, FirstDigit, SecondDigit, ThirdDigit;
+    final int MAGIC_NUMBER = 1089;
+    int Number, ReversedNumber, Difference, FirstDigit, SecondDigit, ThirdDigit,
+        DifferenceFirstDigit, DifferenceSecondDigit, DifferenceThirdDigit,
+        ReversedDifference;
 
-    System.out.println("Please enter a three digit number without trailing zeros when the first digit and last are different from each other: ");
+    System.out.println("Welcome! Please enter a number according to next rules:");
+    System.out.println("1. Enter a number between 100 to 998");
+    System.out.println("2. Enter a non-number palindrome. The first digit and the last must be difference.");
+    System.out.println("Please enter the number:");
 
     Number = scan.nextInt();
 
-    if (Number < 99 || Number > 999) {
+    if (Number < 99 || Number > 998) {
       // Not a three digit number. Display an error and return early for a more
       // readable code.
       System.out.println("The number is not a three digit number!");
@@ -21,6 +27,11 @@ public class Einstein {
     SecondDigit = Number / 10 % 10;
     ThirdDigit = Number % 10;
 
+    if (FirstDigit == ThirdDigit) {
+      System.out.println("The first digit and the last digit are the same. You must a select a non palindrome number.");
+      return;
+    }
+
     ReversedNumber = (ThirdDigit * 100) + (SecondDigit * 10) + FirstDigit;
 
     // Checking which number we need to decrease from each other and then
@@ -28,6 +39,21 @@ public class Einstein {
     Difference = ReversedNumber > Number ? ReversedNumber - Number : Number - ReversedNumber;
 
     System.out.println("The reversed number is: " + ReversedNumber);
-    System.out.println("The difference between the entered number and reversed is: " + Difference);
+    System.out.println("The difference between the entered number and his reversed is: " + Difference);
+
+    // Getting the digits of the difference and build the reversed difference.
+    DifferenceFirstDigit = Difference / 100 % 10;
+    DifferenceSecondDigit = Difference / 10 % 10;
+    DifferenceThirdDigit = Difference % 10;
+    ReversedDifference = (DifferenceThirdDigit * 100) + (DifferenceSecondDigit * 10) + DifferenceFirstDigit;
+
+    System.out.println("The reversed difference is: " + ReversedDifference);
+
+    if (ReversedDifference + Difference == MAGIC_NUMBER) {
+      System.out.println("SUCCEEDED");
+    }
+    else {
+      System.out.println("FAILED");
+    }
   }
 }
